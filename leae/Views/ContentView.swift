f8352726dc0e8b1bec5fc928be6ed21101642330
ErikaHacimoto <erika.t.s.hacimoto@gmail.com>
2025-04-28 11:54:42 -0300

    ContentView

--- leae/Views/ContentView.swift
+++ leae/Views/ContentView.swift
@@ -3,7 +3,19 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        Text("Tela de inicio com logo")
+        VStack {
+            Spacer()
+            Image("App")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 500, height: 500)
+            .shadow(color: .black.opacity(0.4), radius: 5, x: 0, y: 10)
+//            Spacer()
+//            Text("Leae")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("laranja"))
+        .ignoresSafeArea()
     }
 }
 
