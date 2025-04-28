bb2d589c1621c3408414261340750ed6cbe51f37
ErikaHacimoto <erika.t.s.hacimoto@gmail.com>
2025-04-28 12:06:58 -0300

    ContentView

---
+++ leae/Views/ViewAtualizacao.swift
@@ -0,0 +1,20 @@
+//
+//  ViewAtualizacao.swift
+//  leae
+//
+//  Created by Aluno 09 on 28/04/25.
+//
+
+import SwiftUI
+
+struct ViewAtualizacao: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewAtualizacao_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewAtualizacao()
+    }
+}
