a470a98d5d911d36798b8adc06e0380a1b371bb9
ErikaHacimoto <erika.t.s.hacimoto@gmail.com>
2025-04-30 17:41:56 -0300

    Missoes

--- leae/Views/ViewMissoes.swift
+++ leae/Views/ViewMissoes.swift
@@ -45,6 +45,10 @@ struct ConstanciaPresenter<Content: View>: UIViewControllerRepresentable {
 
 struct CustomConstanciaView: View {
     let dias = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"]
+    @State private var resultado_rdc: String = "01"
+    @State private var resultado_rsc: String = "02"
+    @State private var resultado_dc: String = "038"
+    @State private var resultado_sc: String = "04"
     
     var body: some View {
         VStack{
@@ -82,33 +86,194 @@ struct CustomConstanciaView: View {
                         .cornerRadius(10)
                         .padding()
                     Text("Esta Semana")
+                        .font(
+                            Font.custom("SF Pro Text", size: 20)
+                                .weight(.semibold)
+                        )
+                        .multilineTextAlignment(.leading)
                         .foregroundColor(Color("Highlight"))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .offset(x: 30, y: -40)
                 }
                 ZStack {
+                    
                     Rectangle()
                         .foregroundColor(.clear)
                         .frame(width: 344, height: 300)
                         .background(Color("Background"))
                         .cornerRadius(10)
+                    ZStack{
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width:  148, height: 150)
+                            .background(Color("SecundaryDefault"))
+                            .cornerRadius(10)
+                            .offset(x: -83, y: 50)
+                        Text("Semanas Consecutivas")
+                            .font(
+                                Font.custom("SF Pro Text", size: 16)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: 148, alignment: .center)
+                            .offset(x: -85, y: 10)
+                        Text("\(resultado_sc)")
+                            .font(
+                                Font.custom("SF Pro Text", size: 64)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: 130, alignment: .leading)
+                            .offset(x: -60, y: 75)
+                    }
+                    ZStack{
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width:  148, height: 150)
+                            .background(Color("SecundaryDefault"))
+                            .cornerRadius(10)
+                            .offset(x: 83, y: 50)
+                        Text("Dias Consecutivos")
+                            .font(
+                                Font.custom("SF Pro Text", size: 16)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: 130, alignment: .center)
+                            .offset(x: 85, y: 10)
+                        Text("\(resultado_dc)")
+                            .font(
+                                Font.custom("SF Pro Text", size: 64)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: 130, alignment: .center)
+                            .offset(x: 85, y: 75)
+                    }
                     Text("Ofensivas Atuais")
+                        .font(
+                            Font.custom("SF Pro Text", size: 32)
+                                .weight(.semibold)
+                        )
+                        .multilineTextAlignment(.leading)
                         .foregroundColor(Color("Highlight"))
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .offset(y: -65)
                 }
             }
             Spacer().frame(height: 20)
             ScrollView(.horizontal){
                 HStack{
                     Spacer().frame(width: 10)
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 324, height: 185)
-                        .background(Color("Background"))
-                        .cornerRadius(10)
-                        .padding(5)
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 324, height: 185)
-                        .background(Color("Background"))
-                        .cornerRadius(10)
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 324, height: 185)
+                            .background(Color("Background"))
+                            .cornerRadius(10)
+                            .padding(5)
+                        Text("Recorde de Semanas")
+                            .font(
+                                Font.custom("SF Pro Text", size: 28)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .offset(x: 25, y: -60)
+                        Text("Consecutivas")
+                            .font(
+                                Font.custom("SF Pro Text", size: 28)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .offset(x: 25, y: -30)
+                        Text("\(resultado_rsc)")
+                            .font(
+                                Font.custom("SF Pro Text", size: 64)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .offset(x: 25, y: 15)
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.clear)
+                                .frame(width: 324, height: 45)
+                                .background(Color("SecundaryDefault"))
+                                .cornerRadius(10)
+                                .padding(5)
+                                .offset(y:70)
+                            Text("6 de julho - 4 de setembro")
+                                .font(
+                                    Font.custom("SF Pro Text", size: 20)
+                                        .weight(.bold)
+                                )
+                                .multilineTextAlignment(.leading)
+                                .foregroundColor(Color("Highlight"))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .offset(x: 25, y: 68)
+                        }
+                    }
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 324, height: 185)
+                            .background(Color("Background"))
+                            .cornerRadius(10)
+                        Text("Recorde de Dias")
+                            .font(
+                                Font.custom("SF Pro Text", size: 28)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .offset(x: 25, y: -60)
+                        Text("Consecutivos")
+                            .font(
+                                Font.custom("SF Pro Text", size: 28)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .offset(x: 25, y: -30)
+                        Text("\(resultado_rdc)")
+                            .font(
+                                Font.custom("SF Pro Text", size: 64)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color("Highlight"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .offset(x: 25, y: 15)
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.clear)
+                                .frame(width: 324, height: 45)
+                                .background(Color("SecundaryDefault"))
+                                .cornerRadius(10)
+                                .padding(5)
+                                .offset(y:70)
+                            Text("6 de julho - 4 de setembro")
+                                .font(
+                                    Font.custom("SF Pro Text", size: 20)
+                                        .weight(.bold)
+                                )
+                                .multilineTextAlignment(.leading)
+                                .foregroundColor(Color("Highlight"))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .offset(x: 25, y: 68)
+                        }
+                    }
                 }
             }
             Spacer()
