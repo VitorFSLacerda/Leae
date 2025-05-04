a388978d987781c1d2a61e98bfece05ce123acfa
Gabriel Rugeri <gabriel.rugeri@outlook.com>
2025-05-04 14:22:28 -0300

    redefining 'grupo' class

--- leae/Models/Grupo.swift
+++ leae/Models/Grupo.swift
@@ -1,29 +1,59 @@
 import Foundation
 
 class Grupo {
-    
-    private var _livroGrupo: Livro
-    private var _usuariosGrupo: [Usuario]
-    private var _missao: [Missao]
-    
-    init(livroGrupo: Livro, usuariosGrupo: [Usuario], missao: [Missao]) {
-        self._livroGrupo = livroGrupo
-        self._usuariosGrupo = usuariosGrupo
-        self._missao = missao
+    // Propriedades privadas
+    private var _nome: String
+    private var _descricao: String
+    private var _imagem: String
+    private var _livro: Livro
+    private var _usuarios: [Usuario]
+
+    // Construtor
+    init(nome: String, descricao: String, livro: Livro, usuarios: [Usuario] = [], imagem: String) {
+        self._nome = nome
+        self._descricao = descricao
+        self._livro = livro
+        self._usuarios = usuarios
+        self._imagem = imagem
     }
-    
-    var livroGrupo: Livro {
-        get { return _livroGrupo }
-        set { _livroGrupo = newValue }
+
+    // Getter e Setter para `_nome`
+    var nome: String {
+        get { return _nome }
+        set { _nome = newValue }
     }
-    
-    var usuariosGrupo: [Usuario] {
-        get { return _usuariosGrupo }
-        set { _usuariosGrupo = newValue }
+
+    // Getter e Setter para `_descricao`
+    var descricao: String {
+        get { return _descricao }
+        set { _descricao = newValue }
     }
-    
-    var missao: [Missao] {
-        get { return _missao }
-        set { _missao = newValue }
+
+    // Getter e Setter para `_livro`
+    var livro: Livro {
+        get { return _livro }
+        set { _livro = newValue }
+    }
+
+    // Getter e Setter para `_usuarios`
+    var usuarios: [Usuario] {
+        get { return _usuarios }
+        set { _usuarios = newValue }
+    }
+
+    // Getter e Setter para `_imagem`
+    var imagem: String {
+        get { return _imagem }
+        set { _imagem = newValue }
+    }
+
+    // Método para adicionar um usuário ao grupo
+    func adicionarUsuario(_ usuario: Usuario) {
+        _usuarios.append(usuario)
+    }
+
+    // Método para remover um usuário do grupo
+    func removerUsuario(_ usuario: Usuario) {
+        _usuarios = _usuarios.filter { $0 !== usuario } // Remove o usuário se ele existir
     }
 }
